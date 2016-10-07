eb8da777d7857d25f26f74b0e3d90ca3e9f7e0e5
supix <esposito.marce@gmail.com>
2016-10-07 23:42:51 +0200

    Added Code class. It handles random codes generation. Random ISBN10 code can be generated. ISBN10 test case added as well.

---
+++ src/Faker/Code.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Faker.Extensions;
+
+namespace Faker
+{
+    /// <summary>
+    ///     A collection of Code related resources.
+    /// </summary>
+    /// <threadsafety static="true" />
+    public static class Code
+    {
+        /// <summary>
+        ///     Generates an ISBN-10 Code
+        /// </summary>
+        /// <param name="formallyValid">Indicates whether the generated ISBN must be formally valid or not</param>
+        /// <returns>The generated ISBN-10</returns>
+        /// <see cref="https://en.wikipedia.org/wiki/International_Standard_Book_Number"/>>
+        /// <seealso cref="http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js"/>
+        public static string ISBN10(bool formallyValid = true)
+        {
+            int[] v = new int[9];
+            for (int i = 0; i < 9; i++)
+                v[i] = RandomNumber.Next(0, 10);
+            var checksum = computeChecksumIsbn10(v);
+
+            if (!formallyValid)
+                checksum++; //set wrong checksum 
+
+            var prefix = string.Join(string.Empty, v);
+
+            return prefix + (checksum < 10 ? checksum.ToString() : "X");
+        }
+
+        private static int computeChecksumIsbn10(int[] digits)
+        {
+            int sum = 0;
+            
+            for (var i = 0; i < 9; i++)
+                sum += (10 - i) * digits[i];
+
+            sum *= 10;
+
+            return (sum % 11);
+        }
+    }
+}
---
+++ tests/Faker.Tests/Common/CodeTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Faker.Tests.Common
+{
+    [TestFixture]
+    public class CodeTests
+    {
+        [Test]
+        [Repeat(1000)]
+        public void Should_generate_ISBN10_10_chars_long()
+        {
+            var result = Code.ISBN10();
+            Assert.That(result.Length, Is.EqualTo(10));
+        }
+
+        [Test]
+        [Repeat(1000)]
+        public void Should_generate_ISBN10_as_string_of_numbers_and_a_checksum()
+        {
+            var result = Code.ISBN10();
+            Assert.That(result, Is.StringMatching(@"[0-9]{9}[0-9,X]"));
+        }
+
+        [Test]
+        [Repeat(100)]
+        public void Should_generate_ISBN10_with_valid_checksum()
+        {
+            var result = Code.ISBN10();
+            Assert.IsTrue(isIsbn10ChecksumValid(result));
+        }
+
+        /// <summary>
+        /// Computes checksum validity on a ISBN10
+        /// </summary>
+        /// <param name="isbn">The ISBN to be checked</param>
+        /// <returns>Checksum is valid</returns>
+        /// <see cref="http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js"/>
+        private bool isIsbn10ChecksumValid(string isbn)
+        {
+            var v = new int[10];
+            for (int i = 0; i < 9; i++)
+                v[i] = Convert.ToInt32(isbn[i].ToString());
+
+            v[9] = (isbn[9] == 'X' ? 10 : Convert.ToInt32(isbn[9].ToString()));
+
+            var sum = 0;
+            for (var i = 0; i < 10; i++)
+            {
+                sum += (10 - i) * v[i];
+            }
+
+            return (sum % 11) == 0;
+        }
+    }
+}
