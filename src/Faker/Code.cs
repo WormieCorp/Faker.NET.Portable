4d295ec925a5da340ede016ec45709d26f615ffc
supix <esposito.marce@gmail.com>
2016-10-09 14:59:32 +0200

    Applied changes from upstream

--- src/Faker/Code.cs
+++ src/Faker/Code.cs
@@ -2,149 +2,160 @@
 
 namespace Faker
 {
-    /// <summary>
-    ///     A collection of Code related resources.
-    /// </summary>
-    /// <threadsafety static="true" />
-    public static class Code
-    {
-        /// <summary>
-        ///     Generates an ISBN-10 Code
-        /// </summary>
-        /// <param name="validChecksum">Indicates whether the generated ISBN has a valid checksum or not</param>
-        /// <returns>The generated ISBN-10</returns>
-        /// <remarks>
-        ///     Description of the ISBN standard is at https://en.wikipedia.org/wiki/International_Standard_Book_Number
-        ///     Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
-        /// </remarks>
-        public static string ISBN10(bool validChecksum = true)
-        {
-            int[] v = new int[9];
-            for (int i = 0; i < 9; i++)
-                v[i] = RandomNumber.Next(0, 10);
-            var checksum = computeChecksumIsbn10(v);
+	/// <summary>
+	///   A collection of Code related resources.
+	/// </summary>
+	/// <threadsafety static="true" />
+	public static class Code
+	{
+		/// <summary>
+		///   Generates a EAN Code
+		/// </summary>
+		/// <param name="validChecksum">
+		///   Indicates whether the generated EAN has a valid checksum or not
+		/// </param>
+		/// <returns>The generated EAN</returns>
+		/// <remarks>
+		///   Description of the EAN standard is at
+		///   https://en.wikipedia.org/wiki/International_Article_Number Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
+		/// </remarks>
+		public static string EAN(bool validChecksum = true)
+		{
+			int[] v = new int[12];
+			for (int i = 0; i < 12; i++)
+				v[i] = RandomNumber.Next(0, 10);
 
-            if (!validChecksum)
-                checksum = (checksum + 1) % 11; //set wrong checksum
+			var checksum = ComputeChecksumEan(v);
 
-            var prefix = string.Join(string.Empty, v);
+			if (!validChecksum)
+				checksum = (checksum + 1) % 10; //set wrong checksum
 
-            return prefix + (checksum < 10 ? checksum.ToString(CultureInfo.InvariantCulture) : "X");
-        }
+			var prefix = string.Join(string.Empty, v);
 
-        /// <summary>
-        ///     Generates an ISBN-13 Code
-        /// </summary>
-        /// <param name="validChecksum">Indicates whether the generated ISBN has a valid checksum or not</param>
-        /// <returns>The generated ISBN-13</returns>
-        /// <remarks>
-        ///     Description of the ISBN standard is at https://en.wikipedia.org/wiki/International_Standard_Book_Number
-        ///     Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
-        /// </remarks>
-        public static string ISBN13(bool validChecksum = true)
-        {
-            int[] v = new int[12];
-            //ISBN13 starts with "978" or "979"
-            v[0] = 9;
-            v[1] = 7;
-            v[2] = RandomNumber.Next(8, 10);
-            for (int i = 3; i < 12; i++)
-                v[i] = RandomNumber.Next(0, 10);
+			return prefix + checksum;
+		}
 
-            var checksum = computeChecksumEan(v); //ISBN13 is an EAN
+		/// <summary>
+		///   Generates an ISBN-10 Code
+		/// </summary>
+		/// <param name="validChecksum">
+		///   Indicates whether the generated ISBN has a valid checksum or not
+		/// </param>
+		/// <returns>The generated ISBN-10</returns>
+		/// <remarks>
+		///   Description of the ISBN standard is at
+		///   https://en.wikipedia.org/wiki/International_Standard_Book_Number Checksum routines are
+		///   at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
+		/// </remarks>
+		public static string ISBN10(bool validChecksum = true)
+		{
+			int[] v = new int[9];
+			for (int i = 0; i < 9; i++)
+				v[i] = RandomNumber.Next(0, 10);
+			var checksum = ComputeChecksumIsbn10(v);
 
-            if (!validChecksum)
-                checksum = (checksum + 1) % 10; //set wrong checksum
+			if (!validChecksum)
+				checksum = (checksum + 1) % 11; //set wrong checksum
 
-            var prefix = string.Join(string.Empty, v);
+			var prefix = string.Join(string.Empty, v);
 
-            return prefix + checksum;
-        }
+			return prefix + (checksum < 10 ? checksum.ToString(CultureInfo.InvariantCulture) : "X");
+		}
 
-        /// <summary>
-        ///     Generates a EAN Code
-        /// </summary>
-        /// <param name="validChecksum">Indicates whether the generated EAN has a valid checksum or not</param>
-        /// <returns>The generated EAN</returns>
-        /// <remarks>
-        ///     Description of the EAN standard is at https://en.wikipedia.org/wiki/International_Article_Number
-        ///     Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
-        /// </remarks>
-        public static string EAN(bool validChecksum = true)
-        {
-            int[] v = new int[12];
-            for (int i = 0; i < 12; i++)
-                v[i] = RandomNumber.Next(0, 10);
+		/// <summary>
+		///   Generates an ISBN-13 Code
+		/// </summary>
+		/// <param name="validChecksum">
+		///   Indicates whether the generated ISBN has a valid checksum or not
+		/// </param>
+		/// <returns>The generated ISBN-13</returns>
+		/// <remarks>
+		///   Description of the ISBN standard is at
+		///   https://en.wikipedia.org/wiki/International_Standard_Book_Number Checksum routines are
+		///   at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
+		/// </remarks>
+		public static string ISBN13(bool validChecksum = true)
+		{
+			int[] v = new int[12];
+			//ISBN13 starts with "978" or "979"
+			v[0] = 9;
+			v[1] = 7;
+			v[2] = RandomNumber.Next(8, 10);
+			for (int i = 3; i < 12; i++)
+				v[i] = RandomNumber.Next(0, 10);
 
-            var checksum = computeChecksumEan(v);
+			var checksum = ComputeChecksumEan(v); //ISBN13 is an EAN
 
-            if (!validChecksum)
-                checksum = (checksum + 1) % 10; //set wrong checksum
+			if (!validChecksum)
+				checksum = (checksum + 1) % 10; //set wrong checksum
 
-            var prefix = string.Join(string.Empty, v);
+			var prefix = string.Join(string.Empty, v);
 
-            return prefix + checksum;
-        }
+			return prefix + checksum;
+		}
 
-        /// <summary>
-        ///     Generates a RUT Code
-        /// </summary>
-        /// <param name="validChecksum">Indicates whether the generated RUT has a valid checksum or not</param>
-        /// <returns>The generated RUT</returns>
-        /// <remarks>
-        ///     Description of the RUT standard is at https://en.wikipedia.org/wiki/National_identification_number#Chile
-        ///     Checksum routines are at http://www.vesic.org/english/blog-eng/net/verifying-chilean-rut-code-tax-number/
-        /// </remarks>
-        public static string RUT(bool validChecksum = true)
-        {
-            int[] v = new int[8];
+		/// <summary>
+		///   Generates a RUT Code
+		/// </summary>
+		/// <param name="validChecksum">
+		///   Indicates whether the generated RUT has a valid checksum or not
+		/// </param>
+		/// <returns>The generated RUT</returns>
+		/// <remarks>
+		///   Description of the RUT standard is at
+		///   https://en.wikipedia.org/wiki/National_identification_number#Chile Checksum routines
+		///   are at http://www.vesic.org/english/blog-eng/net/verifying-chilean-rut-code-tax-number/
+		/// </remarks>
+		public static string RUT(bool validChecksum = true)
+		{
+			int[] v = new int[8];
 
-            for (int i = 0; i < 8; i++)
-                v[i] = RandomNumber.Next(0, 10);
+			for (int i = 0; i < 8; i++)
+				v[i] = RandomNumber.Next(0, 10);
 
-            var checksum = computeRutChecksum(v);
+			var checksum = ComputeRutChecksum(v);
 
-            if (!validChecksum)
-                checksum = (checksum + 1) % 11; //set wrong checksum
+			if (!validChecksum)
+				checksum = (checksum + 1) % 11; //set wrong checksum
 
-            var prefix = string.Join(string.Empty, v);
+			var prefix = string.Join(string.Empty, v);
 
-            return prefix + (checksum < 10 ? checksum.ToString(CultureInfo.InvariantCulture) : "K");
-        }
+			return prefix + (checksum < 10 ? checksum.ToString(CultureInfo.InvariantCulture) : "K");
+		}
 
-        private static int computeChecksumIsbn10(int[] digits)
-        {
-            int sum = 0;
+		private static int ComputeChecksumEan(int[] digits)
+		{
+			var sum = 0;
+			for (int i = 0; i < 12; i += 2)
+				sum += digits[i];
+			for (int i = 1; i < 12; i += 2)
+				sum += 3 * digits[i];
 
-            for (var i = 0; i < 9; i++)
-                sum += (10 - i) * digits[i];
+			return (10 - (sum % 10)) % 10;
+		}
 
-            sum *= 10;
+		private static int ComputeChecksumIsbn10(int[] digits)
+		{
+			int sum = 0;
 
-            return sum % 11;
-        }
+			for (var i = 0; i < 9; i++)
+				sum += (10 - i) * digits[i];
 
-        private static int computeChecksumEan(int[] digits)
-        {
-            var sum = 0;
-            for (int i = 0; i < 12; i += 2)
-                sum += digits[i];
-            for (int i = 1; i < 12; i += 2)
-                sum += 3 * digits[i];
+			sum *= 10;
 
-            return (10 - (sum % 10)) % 10;
-        }
+			return sum % 11;
+		}
 
-        private static int computeRutChecksum(int[] digits)
-        {
-            int[] coefs = { 3, 2, 7, 6, 5, 4, 3, 2 };
+		private static int ComputeRutChecksum(int[] digits)
+		{
+			int[] coefs = { 3, 2, 7, 6, 5, 4, 3, 2 };
 
-            int sum = 0;
+			int sum = 0;
 
-            for (int i = 0; i < 8; i++)
-                sum += coefs[i] * digits[i];
+			for (int i = 0; i < 8; i++)
+				sum += coefs[i] * digits[i];
 
-            return (11 - (sum % 11)) % 11;
-        }
-    }
+			return (11 - (sum % 11)) % 11;
+		}
+	}
 }
--- tests/Faker.Tests/Common/CodeTest.cs
+++ tests/Faker.Tests/Common/CodeTest.cs
@@ -3,240 +3,228 @@ using System.Text.RegularExpressions;
 
 namespace Faker.Tests.Common
 {
-    [TestFixture]
-    public class CodeTests
-    {
-        #region ISBN10 tests
+	public class CodeTests
+	{
+		#region ISBN10 tests
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN10_10_chars_long()
-        {
-            var result = Code.ISBN10();
-            Assert.That(result.Length, Is.EqualTo(10));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_invalid_ISBN10_with_invalid_checksum()
+		{
+			var result = Code.ISBN10(false);
+			Assert.IsFalse(IsIsbn10ChecksumValid(result), result + " has valid checksum");
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN10_as_string_of_numbers_and_a_checksum()
-        {
-            var result = Code.ISBN10();
-            Assert.That(result, Does.Match(@"[0-9]{9}[0-9,X]"));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN10_10_chars_long()
+		{
+			var result = Code.ISBN10();
+			Assert.That(result.Length, Is.EqualTo(10));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN10_with_valid_checksum()
-        {
-            var result = Code.ISBN10();
-            Assert.IsTrue(IsIsbn10ChecksumValid(result));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN10_as_string_of_numbers_and_a_checksum()
+		{
+			var result = Code.ISBN10();
+			Assert.That(result, Does.Match(@"[0-9]{9}[0-9,X]"));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_invalid_ISBN10_with_invalid_checksum()
-        {
-            var result = Code.ISBN10(false);
-            Assert.IsFalse(IsIsbn10ChecksumValid(result), result + " has valid checksum");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN10_with_valid_checksum()
+		{
+			var result = Code.ISBN10();
+			Assert.IsTrue(IsIsbn10ChecksumValid(result));
+		}
 
-        #endregion ISBN10 tests
+		#endregion ISBN10 tests
 
-        #region ISBN13 tests
+		#region ISBN13 tests
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN13_13_chars_long()
-        {
-            var result = Code.ISBN13();
-            Assert.That(result.Length, Is.EqualTo(13), result + " is not 13 chars long");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_invalid_ISBN13_with_invalid_checksum()
+		{
+			var result = Code.ISBN13(false);
+			Assert.IsFalse(IsEanChecksumValid(result), result.ToString() + " has valid checksum");
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN13_as_string_of_numbers_and_a_checksum()
-        {
-            var result = Code.ISBN13();
-            Assert.That(result, Is.StringMatching(@"[0-9]{13}"));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN13_13_chars_long()
+		{
+			var result = Code.ISBN13();
+			Assert.That(result.Length, Is.EqualTo(13), result + " is not 13 chars long");
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN13_starting_with_978_or_979()
-        {
-            var result = Code.ISBN13();
-            Assert.That(result, Is.StringMatching(@"^97[89]"));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN13_as_string_of_numbers_and_a_checksum()
+		{
+			var result = Code.ISBN13();
+			Assert.That(result, Does.Match(@"[0-9]{13}"));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_ISBN13_with_valid_checksum()
-        {
-            var result = Code.ISBN13();
-            Assert.IsTrue(IsEanChecksumValid(result));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN13_starting_with_978_or_979()
+		{
+			var result = Code.ISBN13();
+			Assert.That(result, Does.Match(@"^97[89]"));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_invalid_ISBN13_with_invalid_checksum()
-        {
-            var result = Code.ISBN13(false);
-            Assert.IsFalse(IsEanChecksumValid(result), result.ToString() + " has valid checksum");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_ISBN13_with_valid_checksum()
+		{
+			var result = Code.ISBN13();
+			Assert.IsTrue(IsEanChecksumValid(result));
+		}
 
-        #endregion ISBN13 tests
+		#endregion ISBN13 tests
 
-        #region EAN tests
+		#region EAN tests
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_EAN_13_chars_long()
-        {
-            var result = Code.EAN();
-            Assert.That(result.Length, Is.EqualTo(13), result + " is not 13 chars long");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_invalid_EAN_with_invalid_checksum()
+		{
+			var result = Code.EAN(false);
+			Assert.IsFalse(IsEanChecksumValid(result), result.ToString() + " has valid checksum");
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_EAN_as_string_of_numbers_and_a_checksum()
-        {
-            var result = Code.EAN();
-            Assert.That(result, Is.StringMatching(@"[0-9]{13}"));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_EAN_13_chars_long()
+		{
+			var result = Code.EAN();
+			Assert.That(result.Length, Is.EqualTo(13), result + " is not 13 chars long");
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_EAN_with_valid_checksum()
-        {
-            var result = Code.EAN();
-            Assert.IsTrue(IsEanChecksumValid(result));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_EAN_as_string_of_numbers_and_a_checksum()
+		{
+			var result = Code.EAN();
+			Assert.That(result, Does.Match(@"[0-9]{13}"));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_invalid_EAN_with_invalid_checksum()
-        {
-            var result = Code.EAN(false);
-            Assert.IsFalse(IsEanChecksumValid(result), result.ToString() + " has valid checksum");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_EAN_with_valid_checksum()
+		{
+			var result = Code.EAN();
+			Assert.IsTrue(IsEanChecksumValid(result));
+		}
 
-        #endregion EAN tests
+		#endregion EAN tests
 
-        #region RUT Tests
+		#region RUT Tests
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_RUT_no_longer_than_9_chars()
-        {
-            var result = Code.RUT();
-            Assert.That(result.Length, Is.LessThanOrEqualTo(9));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_RUT_no_longer_than_9_chars()
+		{
+			var result = Code.RUT();
+			Assert.That(result.Length, Is.LessThanOrEqualTo(9));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_RUT_as_string_of_8_numbers_and_a_checksum()
-        {
-            var result = Code.RUT();
-            Assert.That(result, Does.Match(@"[0-9]{8}[0-9,K]"));
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_RUT_as_string_of_8_numbers_and_a_checksum()
+		{
+			var result = Code.RUT();
+			Assert.That(result, Does.Match(@"[0-9]{8}[0-9,K]"));
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_valid_RUT_with_valid_checksum()
-        {
-            var result = Code.RUT();
-            Assert.IsTrue(IsRutOk(result), "RUT {0} has invalid checksum", result);
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_valid_RUT_with_valid_checksum()
+		{
+			var result = Code.RUT();
+			Assert.IsTrue(IsRutOk(result), "RUT {0} has invalid checksum", result);
+		}
 
-        [Test]
-        [Repeat(1000)]
-        public void Should_generate_invalid_RUT_with_invalid_checksum()
-        {
-            var result = Code.RUT(false);
-            Assert.IsFalse(IsRutOk(result), result + " has valid checksum");
-        }
+		[Test]
+		[Repeat(1000)]
+		public void Should_generate_invalid_RUT_with_invalid_checksum()
+		{
+			var result = Code.RUT(false);
+			Assert.IsFalse(IsRutOk(result), result + " has valid checksum");
+		}
 
-        #endregion RUT Tests
+		#endregion RUT Tests
 
-        #region private convenience methods
+		/// <summary>
+		///   Computes checksum validity on a EAN
+		/// </summary>
+		/// <param name="ean">The EAN to be checked</param>
+		/// <returns>Checksum is valid</returns>
+		/// <remarks>Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js</remarks>
+		private bool IsEanChecksumValid(string ean)
+		{
+			var v = new int[13];
+			for (int i = 0; i < 13; i++)
+				v[i] = (byte)ean[i] - (byte)'0';
 
-        /// <summary>
-        /// Computes checksum validity on a EAN
-        /// </summary>
-        /// <param name="ean">The EAN to be checked</param>
-        /// <returns>Checksum is valid</returns>
-        /// <remarks>
-        ///     Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
-        /// </remarks>
-        private bool IsEanChecksumValid(string ean)
-        {
-            var v = new int[13];
-            for (int i = 0; i < 13; i++)
-                v[i] = (byte)ean[i] - (byte)'0';
+			var sum = 0;
+			for (int i = 0; i < 13; i += 2)
+				sum += v[i];
+			for (int i = 1; i < 13; i += 2)
+				sum += 3 * v[i];
 
-            var sum = 0;
-            for (int i = 0; i < 13; i += 2)
-                sum += v[i];
-            for (int i = 1; i < 13; i += 2)
-                sum += 3 * v[i];
+			return sum % 10 == 0;
+		}
 
-            return sum % 10 == 0;
-        }
+		/// <summary>
+		///   Computes checksum validity on a ISBN10
+		/// </summary>
+		/// <param name="isbn">The ISBN to be checked</param>
+		/// <returns>Checksum is valid</returns>
+		/// <remarks>Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js</remarks>
+		private bool IsIsbn10ChecksumValid(string isbn)
+		{
+			var v = new int[10];
+			for (int i = 0; i < 9; i++)
+				v[i] = (byte)isbn[i] - (byte)'0';
 
-        /// <summary>
-        /// Computes checksum validity on a ISBN10
-        /// </summary>
-        /// <param name="isbn">The ISBN to be checked</param>
-        /// <returns>Checksum is valid</returns>
-        /// <remarks>
-        ///     Checksum routines are at http://www.isbn-check.de/servejs.pl?src=isbnfront.perlserved.js
-        /// </remarks>
-        private bool IsIsbn10ChecksumValid(string isbn)
-        {
-            var v = new int[10];
-            for (int i = 0; i < 9; i++)
-                v[i] = (byte)isbn[i] - (byte)'0';
+			v[9] = (isbn[9] == 'X' ? 10 : (byte)isbn[9] - (byte)'0');
 
-            v[9] = (isbn[9] == 'X' ? 10 : (byte)isbn[9] - (byte)'0');
+			var sum = 0;
+			for (var i = 0; i < 10; i++)
+				sum += (10 - i) * v[i];
 
-            var sum = 0;
-            for (var i = 0; i < 10; i++)
-                sum += (10 - i) * v[i];
+			return (sum % 11) == 0;
+		}
 
-            return (sum % 11) == 0;
-        }
+		/// <summary>
+		///   Computes checksum validity on a RUT
+		/// </summary>
+		/// <param name="isbn">The RUT to be checked</param>
+		/// <returns>Checksum is valid</returns>
+		/// <remarks>Checksum routines are at http://www.vesic.org/english/blog-eng/net/verifying-chilean-rut-code-tax-number/</remarks>
+		private bool IsRutOk(string rut)
+		{
+			if (!Regex.IsMatch(rut, @"[0-9]{8}[0-9,K]"))
+				return false;
 
-        /// <summary>
-        /// Computes checksum validity on a RUT
-        /// </summary>
-        /// <param name="rut">The RUT to be checked</param>
-        /// <returns>Checksum is valid</returns>
-        /// <remarks>
-        ///     Only numbers and optional "K" at the end of string are expected
-        ///     Code available at http://www.vesic.org/english/blog-eng/net/verifying-chilean-rut-code-tax-number/
-        /// </remarks>
-        private bool IsRutOk(string rut)
-        {
-            if (!Regex.IsMatch(rut, @"[0-9]{8}[0-9,K]"))
-                return false;
+			int total = 0;
+			int[] coefs = { 3, 2, 7, 6, 5, 4, 3, 2 };
 
-            int total = 0;
-            int[] coefs = { 3, 2, 7, 6, 5, 4, 3, 2 };
+			for (int i = 0; i < 8; i++)
+				total += coefs[i] * (rut[i] - '0');
 
-            for (int i = 0; i < 8; i++)
-                total += coefs[i] * (rut[i] - '0');
+			int rest = (11 - (total % 11)) % 11;
 
-            int rest = (11 - (total % 11)) % 11;
+			if ((rest == 10) && rut.EndsWith("K"))
+				return true;
 
-            if ((rest == 10) && rut.EndsWith("K"))
-                return true;
+			if (rut[rut.Length - 1] - '0' == rest)
+				return true;
 
-            if (rut[rut.Length - 1] - '0' == rest)
-                return true;
-
-            return false;
-        }
-
-        #endregion private convenience methods
-    }
+			return false;
+		}
+	}
 }
