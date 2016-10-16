580e262a26609dfd96b8c9cc9cd73f5956854139
supix <esposito.marce@gmail.com>
2016-10-16 16:17:03 +0200

    Added data and formats for Italian culture

---
+++ tests/Faker.Tests/it_IT/PhoneItalianTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Faker.Tests.it_IT
+{
+	[SetUICulture("it-IT")]
+	[SetCulture("it-IT")]
+	[Category("Culture 'it_IT'")]
+	public class PhoneItalianTests
+	{
+		[Test]
+		[Repeat(1000)]
+		public void Should_Generate_Cell_Phone_Number()
+		{
+			string expectedFormat = Resources.Phone.CellPhoneFormats.ToFormat();
+
+			string number = Phone.CellNumber();
+
+			number.AssertFormats(expectedFormat);
+		}
+
+		[Test]
+		[Repeat(1000)]
+		public void Should_Generate_Phone_Number()
+		{
+			string expectedFormat = Resources.Phone.Formats.ToFormat();
+
+			string number = Phone.Number();
+
+			number.AssertFormats(expectedFormat);
+		}
+	}
+}
