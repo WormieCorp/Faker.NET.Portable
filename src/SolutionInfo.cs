48e377fc34c70f425c5a2d382091688e7f0147af
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-17 07:08:38 +0200

    Reverted SolutionInfo to previous published information

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,6 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Faker.NET (Portable Edition)")]
 [assembly: AssemblyCopyright("Copyright © 2012-2016")]
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
-[assembly: AssemblyInformationalVersion("2.4.0-build-script.1+127.Branch.build-script.Sha.130d48d38d615357c2150d459537188fe618412d")]
+[assembly: AssemblyVersion("2.3.0.67")]
+[assembly: AssemblyFileVersion("2.3.0.67")]
