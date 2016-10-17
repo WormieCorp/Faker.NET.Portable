10a9e9a0da9a65813dd08d96b4c84973f54d13a7
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-17 07:48:25 +0200

    Created Beer Generator
    Closes #31

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,5 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Faker.NET (Portable Edition)")]
 [assembly: AssemblyCopyright("Copyright © 2012-2016")]
-[assembly: AssemblyVersion("2.3.0.67")]
-[assembly: AssemblyFileVersion("2.3.0.67")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+
+[assembly: AssemblyInformationalVersion("2.4.0-unstable.129+Branch.develop.Sha.552d618817a18cd72fb3dab01f91ce1a864ec4a1")]
