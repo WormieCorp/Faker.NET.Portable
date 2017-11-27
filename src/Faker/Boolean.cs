4dddb26c390e7f032753e84915e288425a49914a
Sparhawk9 <willbkerr@gmail.com>
2017-11-26 20:24:54 -0500

    Add Random Bool

---
+++ src/Faker/Boolean.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Faker
+{
+	/// <summary>
+	///   A collection of Boolean related resources.
+	/// </summary>
+	/// <threadsafety static="true" />
+	public static class Boolean
+	{
+		/// <summary>
+		///   Generates a random Boolean value
+		/// </summary>
+		/// <returns></returns>
+		public static bool Next()
+		{
+			return RandomNumber.Next(0, 2) == 0;
+		}
+	}
+}
