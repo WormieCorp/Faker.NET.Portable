fb75948fc62900d26063f4b2cf9cd79508fe7083
supix <esposito.marce@gmail.com>
2017-11-28 22:01:52 +0100

    Weighted random boolean generation.
    The random Boolean generator has a Next(double) method overload
    with an explicit probability to get a true value.

--- src/Faker/Boolean.cs
+++ src/Faker/Boolean.cs
@@ -1,20 +1,30 @@
-﻿using System;
-
-namespace Faker
+﻿namespace Faker
 {
-	/// <summary>
-	///   A collection of Boolean related resources.
-	/// </summary>
-	/// <threadsafety static="true" />
-	public static class Boolean
-	{
-		/// <summary>
-		///   Generates a random Boolean value
-		/// </summary>
-		/// <returns></returns>
-		public static bool Next()
-		{
-			return RandomNumber.Next(0, 2) == 0;
-		}
-	}
+    /// <summary>
+    ///   A collection of Boolean related resources.
+    /// </summary>
+    /// <threadsafety static="true" />
+    public static class Boolean
+    {
+        /// <summary>
+        ///   Generates a random Boolean value
+        /// </summary>
+        /// <returns>The random boolean value</returns>
+        public static bool Next()
+        {
+            return RandomNumber.Next(0, 2) == 0;
+        }
+
+        /// <summary>
+        ///   Generates a weighted Boolean value
+        /// </summary>
+        /// <param name="trueProbability">
+        ///   The probability to have a true value. Ranges from 0.0 to 1.0.
+        /// </param>
+        /// <returns>The random boolean value</returns>
+        public static bool Next(double trueProbability)
+        {
+            return RandomNumber.NextDouble() < trueProbability;
+        }
+    }
 }
