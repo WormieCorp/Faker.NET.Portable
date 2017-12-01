bf788ef92ebbc0cfbab10497794bd64ead67cdbf
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-12-01 18:23:52 +0100

    (GH-47) Added ability to use https for url
    fixes #47

--- src/Faker/Avatar/FlatHash.cs
+++ src/Faker/Avatar/FlatHash.cs
@@ -2,53 +2,68 @@
 
 namespace Faker
 {
-	/// <summary>
-	///   What kind of image will be generated.
-	/// </summary>
-	/// <remarks>When version 3.0 hits, this will be in Faker.Avatar namespace.</remarks>
-	public enum FlatHashImageFormat
-	{
-		/// <summary>
-		///   A PNG image
-		/// </summary>
-		png,
+    /// <summary>
+    ///   What kind of image will be generated.
+    /// </summary>
+    /// <remarks>When version 3.0 hits, this will be in Faker.Avatar namespace.</remarks>
+    public enum FlatHashImageFormat
+    {
+        /// <summary>
+        ///   A PNG image
+        /// </summary>
+        png,
 
-		/// <summary>
-		///   A JPG image
-		/// </summary>
-		jpg,
+        /// <summary>
+        ///   A JPG image
+        /// </summary>
+        jpg,
 
-		/// <summary>
-		///   A BMP image
-		/// </summary>
-		bmp,
+        /// <summary>
+        ///   A BMP image
+        /// </summary>
+        bmp,
 
-		/// <summary>
-		///   A GIF image
-		/// </summary>
-		gif
-	}
+        /// <summary>
+        ///   A GIF image
+        /// </summary>
+        gif
+    }
 
-	/// <summary>
-	///   A collection of FlatHash.com avatar related resources.
-	/// </summary>
-	/// <threadsafety static="true" />
-	/// <remarks>When version 3.0 hits, this will be in Faker.Avatar namespace.</remarks>
-	/// <seealso cref="RoboHash" />
-	public static class FlatHash
-	{
-		/// <summary>
-		///   Gets a random FlatHash.com image URL.
-		/// </summary>
-		/// <param name="slug">The optional slug to use instead of generating a new random slug.</param>
-		/// <param name="format">The image format to use.</param>
-		/// <returns>The random image URL.</returns>
-		/// <seealso cref="RoboHash.Image(string, string, RoboHashImageFormat, string)" />
-		public static string Image(string slug = null, FlatHashImageFormat format = FlatHashImageFormat.png)
-		{
-			slug = slug ?? string.Join(string.Empty, Lorem.Words(3));
+    /// <summary>
+    ///   A collection of FlatHash.com avatar related resources.
+    /// </summary>
+    /// <threadsafety static="true" />
+    /// <remarks>When version 3.0 hits, this will be in Faker.Avatar namespace.</remarks>
+    /// <seealso cref="RoboHash" />
+    public static class FlatHash
+    {
+        /// <summary>
+        ///   Gets a random FlatHash.com image URL.
+        /// </summary>
+        /// <param name="slug">The optional slug to use instead of generating a new random slug.</param>
+        /// <param name="format">The image format to use.</param>
+        /// <returns>The random image URL.</returns>
+        /// <seealso cref="RoboHash.Image(string, string, RoboHashImageFormat, string)" />
+		/// <seealso cref="Image(bool,string,FlatHashImageFormat)" />
+        public static string Image(string slug = null, FlatHashImageFormat format = FlatHashImageFormat.png)
+        {
+            return Image(false, slug: slug, format: format);
+        }
 
-			return "http://flathash.com/{0}.{1}".FormatCulture(slug, format);
-		}
-	}
+        /// <summary>
+        ///   Gets a random FlatHash.com image URL.
+        /// </summary>
+        /// <param name="useSsl">Set to <c>true</c> if https should be used, otherwise <c>false</c></param>
+        /// <param name="slug">The optional slug to use instead of generating a new random slug.</param>
+        /// <param name="format">The image format to use.</param>
+        /// <returns>The random image URL.</returns>
+        /// <seealso cref="RoboHash.Image(string, string, RoboHashImageFormat, string)" />
+        /// <seealso cref="Image(string,FlatHashImageFormat)" />
+        public static string Image(bool useSsl, string slug = null, FlatHashImageFormat format = FlatHashImageFormat.png)
+        {
+            slug = slug ?? string.Join(string.Empty, Lorem.Words(3));
+
+            return "{0}://flathash.com/{1}.{2}".FormatCulture(useSsl ? "https" : "http", slug, format);
+        }
+    }
 }
